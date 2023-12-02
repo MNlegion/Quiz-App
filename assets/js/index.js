4aa557fb25798eb402edd16229f6d1f0cfd031c5
MNlegion <rkreuser30@gmail.com>
2023-12-02 14:52:12 -0500

    initial game functionality setup

--- assets/js/index.js
+++ assets/js/index.js
@@ -45,3 +45,32 @@ const questions = [
         ]
     }
 ]
+
+const startButton = document.getElementById("start-btn");
+const nextButton = document.getElementById("next-btn");
+const questionContainerElement = document.getElementById("question-container");
+const questionElement = document.getElementById("question");
+
+const answerButtonsElement = document.getElementById("answer-buttons");
+
+let shuffledQuestions, currentQuestionIndex;
+
+startButton.addEventListener("click", startGame);
+nextButton.addEventListener("click", () => {
+    currentQuestionIndex++;
+    setNextQuestion();
+});
+
+function startGame() {
+    console.log("Started");
+    startButton.classList.add("hide");
+    shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+    currentQuestionIndex = 0;
+    questionContainerElement.classList.remove("hide");
+    setNextQuestion();
+};
+
+function setNextQuestion() {
+    resetState();
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
+};
